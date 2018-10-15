2e4721952c6c270fdd0c87367af3cf952abec390
Caroline Chocholak <carolinechocholak@usc-guestwireless-upc-newsc21479.usc.edu>
2018-10-14 18:18:39 -0700

    merged

--- app/src/main/java/Controller/KillDDLController.java
+++ app/src/main/java/Controller/KillDDLController.java
@@ -1,4 +1,51 @@
 package Controller;
 
 public class KillDDLController {
+
+    /**
+     * Tentative member variables we will have, add more if needed
+     */
+
+//    private User currentUser;
+//    private MainView mainView;
+//    private MonthlyView monthlyView;
+//    private DailyView dailyView;
+//    private DeadlineView deadlineView;
+//    private EditView editView;
+
+    /**
+     * Tentative classes for the controller, add more if needed
+     *
+     */
+
+//    public void removeDeadline(Deadline deadline) {
+//
+//    }
+//
+//    public void addDeadline(Deadline deadline) {
+//
+//    }
+//
+//    public void validateLogin(String username, String password) {
+//
+//    }
+//
+//    public void openMainView() {
+//
+//    }
+//
+//    public void openDeadlineView() {
+//
+//    }
+//
+//    public void openEditView() {
+//
+//    }
+//
+//    public void openDailyView() {
+//
+//    }
+
+
+
 }
