7e08c6c0d117e1d7a681ee6bc40dc3ab9c621306
Caroline Chocholak <carolinechocholak@guest-wireless-upc-1600-10-120-011-045.usc.edu>
2018-10-17 10:27:57 -0700

    added calendar view

--- app/src/main/java/com/example/emmamalysz/killddl/CalendarActivity.java
+++ app/src/main/java/com/example/emmamalysz/killddl/CalendarActivity.java
@@ -1,27 +1,54 @@
 package com.example.emmamalysz.killddl;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CalendarActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView myDate;
+    ListView deadlineList;
+    List<String> ddls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.myDate);
+        deadlineList = (ListView) findViewById(R.id.deadlineList);
+        String [] deadlines = new String[] {
+                "ITP Exam",
+                "Interview",
+                "Electricity Bill"
+        };
+        ddls = new ArrayList<>(Arrays.asList(deadlines));
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, ddls);
+        deadlineList.setAdapter(arrayAdapter);
+
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = (dayOfMonth) + " " + (month + 1) + " " + (year);
+                myDate.setText(date);
+                ddls.add(date);
+                arrayAdapter.notifyDataSetChanged();
             }
         });
     }
