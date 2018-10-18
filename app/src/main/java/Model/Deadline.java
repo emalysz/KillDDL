49230e915cda40bbfb6f245d0ec7545cba4b59eb
Eric Su <esu@usc.edu>
2018-10-18 16:59:06 -0700

    color parameter

--- app/src/main/java/Model/Deadline.java
+++ app/src/main/java/Model/Deadline.java
@@ -12,13 +12,15 @@ public class Deadline implements Serializable {
     private Date date;
     private int priority;
     private int notification;
+    private int color;
 
-    public Deadline(String _title, String _description, Date _date, int _priority, int _notification){
+    public Deadline(String _title, String _description, Date _date, int _priority, int _notification, int _color){
         title = _title;
         description = _description;
         date = _date;
         priority = _priority;
         notification = _notification;
+        color = _color;
     }
 
 
