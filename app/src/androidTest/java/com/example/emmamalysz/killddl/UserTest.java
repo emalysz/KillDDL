2de1b55aa5dc88adbcc7581e915c4158cf10b4e5
Jen Anthony <anth595@usc.edu>
2018-10-28 20:53:49 -0700

    added a couple more tests to User test

--- app/src/androidTest/java/com/example/emmamalysz/killddl/UserTest.java
+++ app/src/androidTest/java/com/example/emmamalysz/killddl/UserTest.java
@@ -26,16 +26,16 @@ public class UserTest {
                 2, 1, 2, 0);
         user0.addDeadline(d);
         assertTrue(!user0.getDeadlines().equals(null));
+        assertEquals(1, user0.getDeadlines().size());
 
-
-
+//      Tests that deadlines with a daily frequency are adding one year's worth of deadlines
         User user1 = new User("freqDaily", "jen@gmail.com", "test123", 2746467);
         Deadline ddlD = new Deadline("BUAD 311 HW", "Finish pre-class assignment", new Date(11-04-2108),
                 2, 1, 0, 1);
         user1.addDeadline(ddlD);
         assertEquals(366, user1.getDeadlines().size());
 
-
+//      Tests that deadlines with a monthly frequency are adding one year's worth of deadlines
         User user2 = new User("freqMonthly", "jen@gmail.com", "test123", 2746467);
         Deadline ddlM = new Deadline("PYSC 165 Quiz", "Finish pre-class assignment", new Date(11-04-2108),
                 2, 1, 0, 2);
@@ -53,6 +53,16 @@ public class UserTest {
 
         assertTrue(user.getDeadlines().isEmpty());
 
+        Deadline ddl2 = new Deadline("BUAD 311 Midterm", "Study guide", Calendar.getInstance().getTime(),
+                2, 1, 2, 0);
+        user.addDeadline(d);
+        user.addDeadline(ddl2);
+        user.removeDeadline(d);
+
+        assertEquals(1, user.getDeadlines().size());
+        assertEquals("BUAD 311 Midterm", user.getDeadlines().get(0).getTitle());
+
+
     }
 
     @Test
@@ -61,6 +71,7 @@ public class UserTest {
         Deadline d = new Deadline("Finish Project 2.4", "Test cases", Calendar.getInstance().getTime(),
                 2, 1, 2, 0);
         user.addDeadline(d);
+        assertTrue(!user.getDeadlines().get(0).isCompleted());
         user.setDeadlineComplete(d);
         assertTrue(user.getDeadlines().get(0).isCompleted());
 
