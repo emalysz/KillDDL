01b2ec300ae7c07734cf97647bce141dad86ad19
Eric Su <esu@usc.edu>
2018-10-28 23:08:13 -0700

    edit deadline nominal and edge case

--- app/src/androidTest/java/com/example/emmamalysz/killddl/EditDeadlineViewTests.java
+++ app/src/androidTest/java/com/example/emmamalysz/killddl/EditDeadlineViewTests.java
@@ -1,8 +1,59 @@
 package com.example.emmamalysz.killddl;
 
-/**
- * Created by reysu on 10/28/18.
- */
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Calendar;
+
+import static android.support.test.InstrumentationRegistry.getTargetContext;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.action.ViewActions.swipeRight;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(AndroidJUnit4.class)
 public class EditDeadlineViewTests {
+
+    @Rule
+    public ActivityTestRule<EditDeadlineActivity> mActivityTestRule = new ActivityTestRule<EditDeadlineActivity>(EditDeadlineActivity.class);
+
+    @Test
+    public void EditDeadlineTests() {
+        Espresso.onView(withId(R.id.deadlineName)).perform(scrollTo(), replaceText("Test Deadline"), closeSoftKeyboard());
+        Espresso.onView(withId(R.id.deadlineDescription)).perform(scrollTo(), replaceText("Deadline Description"), closeSoftKeyboard());
+        Espresso.onView(withId(R.id.addDeadline)).perform(scrollTo(),click());
+        //  Espresso.onView(withId(R.id.daily_view)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void DeadlineTitleCannotBeBlank() {
+        Espresso.onView(withId(R.id.addDeadline)).perform(scrollTo(),click());
+        Espresso.onView(withId(R.id.edit_deadline_activity)).check(matches(isDisplayed()));
+    }
+
+
+
 }
