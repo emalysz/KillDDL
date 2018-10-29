497ecef3122a6c9b4f53b089223fb52f4268d6e8
Eric Su <esu@usc.edu>
2018-10-29 10:28:02 -0700

    bug in empty deadline name

--- app/src/main/java/com/example/emmamalysz/killddl/EditDeadlineActivity.java
+++ app/src/main/java/com/example/emmamalysz/killddl/EditDeadlineActivity.java
@@ -109,7 +109,7 @@ public class EditDeadlineActivity extends AppCompatActivity {
         addDeadline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(deadlineName != null){
+                if(deadlineName == null){
                     deadlineName.setError("Title cannot be blank");
                 }else {
                     Deadline newDeadline = d;
